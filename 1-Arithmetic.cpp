feb2349d8e5651ca88a87a7c04bcdabc57763de3
mrtsyg <103937490+mrtsyg@users.noreply.github.com>
2023-09-19 14:29:00 +0300

    firstnoteadded

---
+++ 1-Arithmetic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int firstNum = 12;
+    int secondNum = 13;
+    int addition = firstNum + secondNum;
+    int subtraction = firstNum - secondNum;
+    int multiplication = firstNum * secondNum;
+    cout << addition << endl;
+    cout << subtraction << endl;
+    cout << multiplication << endl;
+    return 0;
+}
